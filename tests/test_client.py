6a39fb5613a1553046465d95c65927592c90e513
Constance Martineau <constance.alyssa.martineau@gmail.com>
2021-05-08 16:55:32 -0400

    Simplify test, and rename for clarity

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,28 +3,27 @@ import pytest
 import client
 
 
-my_var_foo = {"MY_VAR": {"key": "MY_VAR", "value": "foo", "is_secret": False}}
-my_var_bar = {"MY_VAR": {"key": "MY_VAR", "value": "bar", "is_secret": False}}
-another_var = {"ANOTHER_VAR": {"key": "ANOTHER_VAR", "value": "foo", "is_secret": False}}
-two_vars = {
-    "MY_VAR": {"key": "MY_VAR", "value": "foo", "is_secret": False},
-    "ANOTHER_VAR": {"key": "ANOTHER_VAR", "value": "foo", "is_secret": False},
-}
-no_var = None
-empty_dict = {}
+foo = {"key": "FOO", "value": "foo", "is_secret": False}
+bar = {"key": "FOO", "value": "bar", "is_secret": False}
+baz = {"key": "BAZ", "value": "baz", "is_secret": False}
+FOO = {"FOO": foo}
+BAR = {"FOO": bar}
+BAZ = {"BAZ": baz}
+FOO_BAZ = {"FOO": foo, "BAZ": baz}
 
+# (actual, desired, expected_to_add, expected_to_delete)
 compare_test_data = [
-    (my_var_foo, my_var_foo, empty_dict, empty_dict), # nothing changes
-    (my_var_foo, my_var_bar, my_var_foo, my_var_bar), # env var value changes
-    (my_var_foo, two_vars, empty_dict, another_var), # 2 env vars exist, one to be removed from config
-    (two_vars, my_var_foo, another_var, empty_dict), # one env var exists, one to be added from config
-    (my_var_foo, no_var, my_var_foo, empty_dict), # no env vars exist, one to be added from config
-    (no_var, my_var_foo, empty_dict, my_var_foo), # one env var exists, none listed in config
+    (FOO, FOO, {}, {}),  # nothing changes, add:0 del:0
+    (FOO, BAR, BAR, FOO),  # env var value changes, add:1 del:1
+    (FOO_BAZ, FOO, {}, BAZ),  # two env vars, remove 1 env var from config - add:0 - del:1
+    (FOO, FOO_BAZ, BAZ, {}),  # one env var exists, one to be added from config - add:1 del:0
+    (None, FOO, FOO, {}),  # no env vars exist, one to be added from config add:1 del:0
+    (FOO, None, {}, FOO),  # one env var exists, none listed in config
 ]
 
 
-@pytest.mark.parametrize("desired_values,actual_values,values_to_add,values_to_delete", compare_test_data)
-def test_compare(desired_values, actual_values, values_to_add, values_to_delete):
-    results_to_add, results_to_delete = client.compare(actual_values, desired_values)
-    assert results_to_add == values_to_add
-    assert results_to_delete == values_to_delete
+@pytest.mark.parametrize("actual,desired,expected_to_add,expected_to_delete", compare_test_data)
+def test_compare(actual, desired, expected_to_add, expected_to_delete):
+    to_add, to_delete = client.compare(actual, desired)
+    assert to_add == expected_to_add
+    assert to_delete == expected_to_delete

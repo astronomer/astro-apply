2bacffaa13101fa20d0c9cd324cd9c642530b9bb
Constance Martineau <constance.alyssa.martineau@gmail.com>
2021-05-13 20:16:30 -0400

    format

--- client/env_vars.py
+++ client/env_vars.py
@@ -6,29 +6,22 @@ from client import run
 
 
 def apply(deployment, deployment_cfg):
+
     logging.info(
         f"Applying Deployment Environment Variables for {deployment.release_name}..."
     )
-    current_env_vars = {
-        env_var.key: env_var.__json_data__
-        for env_var in deployment.environment_variables
-    }
-    new_env_vars = {
-        env_var["key"]: env_var
-        for env_var in deployment_cfg.get("environmentVariables", [])
-    }
 
-    env_vars_to_add, env_vars_to_update, env_vars_to_delete = compare(
-        current_env_vars, new_env_vars
-    )
+    current, new = parse_env_vars(deployment, deployment_cfg)
+
+    vars_to_add, vars_to_update, vars_to_delete = compare(current, new)
 
     # env vars don't have an add or delete function in houston
     # instead, we will need to pass all values if changes are needed
-    env_vars_to_add.update(env_vars_to_update)
-    logging.debug(f"Adding: {env_vars_to_add}\n Removing: {env_vars_to_delete} ")
-    # if yes, use values directly from config
-    if env_vars_to_add or env_vars_to_delete:
-        update(deployment, new_env_vars)
+    vars_to_add.update(vars_to_update)
+    logging.debug(f"Adding: {vars_to_add}\n Removing: {vars_to_delete} ")
+
+    if vars_to_add or vars_to_delete:
+        update_(deployment, vars_to_add)
 
     else:
         logging.info(
@@ -36,19 +29,31 @@ def apply(deployment, deployment_cfg):
         )
 
 
-def update(deployment, environment_variables):
-    deployment_variables = [
+def parse_env_vars(deployment, deployment_cfg):
+    current = {
+        env_var.key: env_var.__json_data__
+        for env_var in deployment.environment_variables
+    }
+    new = {
+        env_var["key"]: env_var
+        for env_var in deployment_cfg.get("environmentVariables", [])
+    }
+    return current, new
+
+
+def update_(deployment, environment_variables):
+    environment_variables = [
         schema.InputEnvironmentVariable(
             key=value["key"], value=value["value"], is_secret=value["isSecret"]
         )
         for value in environment_variables.values()
     ]
-    update_deployment_variables_args = {
+    deployment_variables = {
         "deployment_uuid": deployment.id,
         "release_name": deployment.release_name,
-        "environment_variables": deployment_variables,
+        "environment_variables": environment_variables,
     }
     run(
-        lambda m: m.update_deployment_variables(**update_deployment_variables_args),
+        lambda m: m.update_deployment_variables(**deployment_variables),
         is_mutation=True,
     )

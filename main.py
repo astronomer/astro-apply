3f23f905e7f40a1d50c9cb10dcc305be4273a5ca
Constance Martineau <constance.alyssa.martineau@gmail.com>
2021-05-20 15:54:15 -0400

    ability to set defaults in config for multiple deploys

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 Run `sh update-schema.sh` to update houston schema
 """
 import logging
+import os
+
+import yaml
 
 import client
 from client import env_vars
@@ -12,10 +15,13 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def main():
-    config = client.load_config()
+    config = load_config(os.environ.get("ASTRO_CONFIG", "config.yaml"))
+    config = client.apply_defaults(config)
+
     deployments = config["deployments"]
 
     for deployment_cfg in deployments:
+
         deployment = client.run(
             lambda q: q.deployment(
                 where=schema.DeploymentWhereUniqueInput(
@@ -28,5 +34,19 @@ def main():
         users.apply(deployment, deployment_cfg)
 
 
+def load_config(config_file) -> dict:
+    if os.path.exists(config_file) is False:
+        raise SystemExit("Config File not found! Exiting!")
+
+    logging.info("Parsing config.yaml...")
+    with open(config_file, "r") as f:
+        config = yaml.safe_load(f)
+
+    return config
+
+
 if __name__ == "__main__":
     main()
+
+
+
